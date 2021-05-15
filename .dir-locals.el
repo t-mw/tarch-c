61a4cf88558c63ec75522f0be3b21b860a4b4fc2
Tobias Mansfield-Williams <contact@tmwhere.com>
2021-05-15 23:52:36 +0200

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode .((projectile-project-compilation-dir . ".")
+            (projectile-project-compilation-cmd . "make"))))

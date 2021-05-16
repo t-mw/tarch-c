92bf0a0414d588a397a1604b196ec00fb2e996ff
Tobias Mansfield-Williams <contact@tmwhere.com>
2021-05-16 21:58:15 +0200

    Use sx allocator in sokol

--- src/tarch/sokol.c
+++ src/tarch/sokol.c
@@ -3,6 +3,12 @@
 #  define SOKOL_LOG(str) OutputDebugStringA(str)
 #endif
 
+#include <sx/allocator.h>
+#define SOKOL_MALLOC(s) sx_malloc(sx_alloc_malloc(), s)
+#define SOKOL_CALLOC(n, s) sx_calloc(sx_alloc_malloc(), n* s)
+#define SOKOL_REALLOC(p, s) sx_realloc(sx_alloc_malloc(), p, s)
+#define SOKOL_FREE(p) sx_free(sx_alloc_malloc(), p)
+
 #include <sokol/sokol_app.h>
 #include <sokol/sokol_audio.h>
 #include <sokol/sokol_gfx.h>

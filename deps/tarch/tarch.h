a7648f94b21f431379ee7e333237a016f05d740b
Tobias Mansfield-Williams <contact@tmwhere.com>
2023-05-22 23:03:30 +0200

    Improve LOG macro

--- deps/tarch/tarch.h
+++ deps/tarch/tarch.h
@@ -16,23 +16,43 @@
     printf("%zu: " format "\n", i, arr[i]);                     \
   }
 
+#include <pthread.h>
 #include <stdio.h>
 #include <time.h>
-#define LOG(tag, ...)                                              \
-  do {                                                             \
-    static char formatted_msg[256];                                \
-    static char time_str[32];                                      \
-    time_t now;                                                    \
-    struct tm* timeinfo;                                           \
-                                                                   \
-    time(&now);                                                    \
-    timeinfo = localtime(&now);                                    \
-                                                                   \
-    strftime(time_str, sizeof(time_str), "%F %H:%M:%S", timeinfo); \
-    snprintf(formatted_msg, sizeof(formatted_msg), __VA_ARGS__);   \
-                                                                   \
-    printf("%s [%s]: %s\n", time_str, tag, formatted_msg);         \
+
+#define LOG_MAX_MSG_LEN 256
+#define LOG_MAX_TIME_LEN 32
+
+#define LOG(tag, ...)                                                          \
+  do {                                                                         \
+    char formatted_msg[LOG_MAX_MSG_LEN];                                       \
+    char time_str[LOG_MAX_TIME_LEN];                                           \
+    time_t now;                                                                \
+    struct tm timeinfo;                                                        \
+                                                                               \
+    time(&now);                                                                \
+    localtime_r(&now, &timeinfo);                                              \
+                                                                               \
+    if (strftime(time_str, sizeof(time_str), "%F %H:%M:%S", &timeinfo) == 0) { \
+      fprintf(stderr, "strftime error\n");                                     \
+      break;                                                                   \
+    }                                                                          \
+                                                                               \
+    int ret = snprintf(formatted_msg, sizeof(formatted_msg), __VA_ARGS__);     \
+    if (ret < 0) {                                                             \
+      perror("snprintf");                                                      \
+      break;                                                                   \
+    } else if (ret >= (int)sizeof(formatted_msg)) {                            \
+      fprintf(stderr, "LOG message too long for buffer\n");                    \
+      break;                                                                   \
+    }                                                                          \
+                                                                               \
+    if (printf("%s [%s]: %s\n", time_str, tag, formatted_msg) < 0) {           \
+      perror("printf");                                                        \
+      break;                                                                   \
+    }                                                                          \
   } while (0)
+
 #if DBG
 #  define DBG_LOG(tag, ...) LOG("debug:" tag, __VA_ARGS__)
 #else

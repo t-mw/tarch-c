ca3458b207a3ab3e5d57968bd94c7373765cd36e
Tobias Mansfield-Williams <contact@tmwhere.com>
2023-11-26 00:49:49 +0100

    Remove sokol

--- deps/sokol.c
+++
@@ -1,9 +0,0 @@
-#define SOKOL_IMPL
-
-#include <sokol/sokol_audio.h>
-#include <sokol/sokol_gfx.h>
-#include <sokol/sokol_glue.h>
-#include <sokol/sokol_log.h>
-#include <sokol/sokol_time.h>
-
-#include <sokol/util/sokol_shape.h>

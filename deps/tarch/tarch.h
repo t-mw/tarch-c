ba84076ca948e31c17c1921a5a2e4257a054b07f
Tobias Mansfield-Williams <contact@tmwhere.com>
2023-12-10 19:57:06 +0100

    Add debug_print

--- deps/tarch/tarch.h
+++ deps/tarch/tarch.h
@@ -58,6 +58,63 @@
 #define debug_log(tag, ...)
 #endif
 
+static inline void _tm_print_char(char v) {
+    printf("%c", v);
+}
+static inline void _tm_print_int(int v) {
+    printf("%d", v);
+}
+static inline void _tm_print_uint(unsigned int v) {
+    printf("%u", v);
+}
+static inline void _tm_print_ushort(unsigned short v) {
+    printf("%hu", v);
+}
+static inline void _tm_print_float(float v) {
+    printf("%f", v);
+}
+static inline void _tm_print_double(double v) {
+    printf("%f", v);
+}
+static inline void _tm_print_long(long v) {
+    printf("%ld", v);
+}
+static inline void _tm_print_ulong(unsigned long v) {
+    printf("%lu", v);
+}
+static inline void _tm_print_llong(long long v) {
+    printf("%lld", v);
+}
+static inline void _tm_print_ullong(unsigned long long v) {
+    printf("%llu", v);
+}
+static inline void _tm_print_ptr(void* v) {
+    printf("%p", v);
+}
+
+#define _tm_generic_print(val)                \
+    _Generic((val),                           \
+        char: _tm_print_char,                 \
+        int: _tm_print_int,                   \
+        unsigned int: _tm_print_uint,         \
+        unsigned short: _tm_print_ushort,     \
+        float: _tm_print_float,               \
+        double: _tm_print_double,             \
+        long: _tm_print_long,                 \
+        unsigned long: _tm_print_ulong,       \
+        long long: _tm_print_llong,           \
+        unsigned long long: _tm_print_ullong, \
+        default: _tm_print_ptr)(val)
+
+#define tm_debug_print(val)                \
+    do {                                   \
+        if (DEBUG) {                       \
+            printf("value of %s: ", #val); \
+            _tm_generic_print(val);        \
+            printf("\n");                  \
+        }                                  \
+    } while (0)
+
 void* tm_malloc(size_t size);
 void* tm_calloc(size_t n, size_t s);
 void* tm_realloc(void* p, size_t s);
